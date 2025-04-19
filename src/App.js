31101fdb8a1aec3821c1424d02f65de6b6a8a47a
wilbertbw <wilbertbwibowo@gmail.com>
2025-04-19 00:05:12 -0400

    moved dummy tasks into a state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import Tasks from './pages/Task';
 
-function App() {  
-  const taskList = [
+function App() {
+  const [taskList, setTaskList] = useState([
     {name: "Do Assignments", description: "Complete assignments", deadline: "17 April 2025", completed: false},
     {name: "Study", description: "Study for exams", deadline: "20 April 2025", completed: false}
-  ];
+  ]);
 
   return (
     <>
