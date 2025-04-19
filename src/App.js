00a1ef53ed9a347a346cd347a6831741f4f45a82
wilbertbw <wilbertbwibowo@gmail.com>
2025-04-19 15:55:15 -0400

    updated modal to use createPortal

--- src/App.js
+++ src/App.js
@@ -13,17 +13,19 @@ function App() {
 
   const [newTaskModalState, setNewTaskModalState] = useState(false);
 
-  function handleAddTask() {
-
-  }
-
   return (
     <>
       <div id="TopBar">
         <p id="TopBarText">Todo List</p>
         <button id="TopBarButton" onClick={() => {setNewTaskModalState(true)}}>New Task</button>
       </div>
-      <NewTaskModal currentModalState={newTaskModalState} setCurrentModalState={setNewTaskModalState}/>
+      <NewTaskModal id="NewTaskModal" open={newTaskModalState} onClose={() => {setNewTaskModalState(false)}}>
+        <p>Name</p>
+        <p>Description</p>
+        <p>Deadline</p>
+        <button onClick={() => {setNewTaskModalState(false)}}>Save Task</button>
+        <button onClick={() => {setNewTaskModalState(false)}}>Cancel</button>
+      </NewTaskModal>
       <Tasks taskList={taskList} setTaskList={setTaskList} completedTasks={completedTasks} setCompletedTasks={setCompletedTasks}/>
     </>
   );
--- src/components/NewTaskModal.jsx
+++ src/components/NewTaskModal.jsx
@@ -1,12 +1,21 @@
-export default function NewTaskModal({ currentModalState, setCurrentModalState }) {
-  if (currentModalState === false) {
-    return null;
-  }
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 
-  return (
-    <div id="ModalContainer">
-      <p>test</p>
-      <button onClick={() => {setCurrentModalState(false)}}>Save Task</button>
-    </div>
-  );
+export default function NewTaskModal({ children, open, onClose, }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    const modal = dialog.current
+    if (open) {
+      modal.showModal();
+    }
+
+    return () => modal.close();
+  }, [open]);
+
+  return createPortal(
+    <dialog id="NewTaskModal" ref={dialog} onClose={onClose}>
+      {children}
+    </dialog>,
+    document.getElementById('modal-root'));
 }
